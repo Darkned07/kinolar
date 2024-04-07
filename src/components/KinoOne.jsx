e9833b943af35bd879cd9ab2385871a487f79cb6
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-04-07 23:20:00 +0500

    update websites one

--- src/components/KinoOne.jsx
+++ src/components/KinoOne.jsx
@@ -20,37 +20,46 @@ function KinoOne() {
     <div className="my-[30px]">
       <div className="card lg:card-side bg-base-100 shadow-xl mb-[10px]">
         <figure>
-          <img className="rounded-[20px]" src={kino.image} alt="Album" />
+          <img
+            className="rounded-[20px]"
+            src={kino && kino.image}
+            alt="Album"
+          />
         </figure>
         <div className="card-body">
-          <h2 className="card-title mx-auto lg:mx-0">{kino.name}</h2>
+          <h2 className="card-title mx-auto lg:mx-0">{kino && kino.name}</h2>
           <div className="flex flex-col gap-2 mt-[10px] mx-auto lg:mx-0">
             <span className="flex flex-wrap gap-2 font-bold">
               Janri:
               <span className="flex capitalize flex-wrap gap-1 font-normal">
-                {kino.janri.map((l) => {
-                  return <p key={l}>{l}</p>;
-                })}
+                {kino &&
+                  kino.janri.map((l) => {
+                    return <p key={l}>{l}</p>;
+                  })}
               </span>
             </span>
             <span className="flex flex-wrap gap-2 font-bold">
-              Tili: <p className="font-normal capitalize">{kino.tili}</p>
+              Tili:{" "}
+              <p className="font-normal capitalize">{kino && kino.tili}</p>
             </span>
             <span className="flex flex-wrap gap-2 font-bold">
               Davlati:
-              <p className="font-normal capitalize">{kino.davlati}</p>
+              <p className="font-normal capitalize">{kino && kino.davlati}</p>
             </span>
             <span className="flex flex-wrap gap-2 font-bold">
-              Yili: <p className="font-normal capitalize">{kino.yili}</p>
+              Yili:{" "}
+              <p className="font-normal capitalize">{kino && kino.yili}</p>
             </span>
             <span className="flex flex-wrap gap-2 font-bold">
               Davomiyligi:
-              <p className="font-normal capitalize">{kino.davomiyligi}</p>
+              <p className="font-normal capitalize">
+                {kino && kino.davomiyligi}
+              </p>
             </span>
           </div>
           <div className="flex flex-col mt-[20px] gap-5">
             <Link
-              to={kino.tgsilka}
+              to={kino && kino.tgsilka}
               target="_blank"
               className="btn btn-success btn-outline "
             >
@@ -59,7 +68,7 @@ function KinoOne() {
             <Link
               className="btn btn-secondary btn-outline"
               target="_blank"
-              to={kino.tgkanali}
+              to={kino && kino.tgkanali}
             >
               Telegram kanalimiz
             </Link>
@@ -73,7 +82,7 @@ function KinoOne() {
       <div className="card flex flex-col lg:card-side bg-base-100 p-[10px] h-[250px]  rounded-[20px] sm:h-[350px] md:h-[500px] shadow-xl">
         <YouTube
           className="w-full h-full"
-          videoId={kino.ytcodes}
+          videoId={kino && kino.ytcodes}
           opts={opts}
           onReady={onReady}
         />
